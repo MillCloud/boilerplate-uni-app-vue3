a10ebe96084be5417c4b419c5b3d637726d6ee54
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-03-14 10:09:42 +0800

    fix: fix warning

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,4 +2,8 @@ const { eslint } = require('@modyqyw/fabric');
 
 module.exports = {
   ...eslint.vue3TypescriptPrettier,
+  rules: {
+    ...eslint.vue3TypescriptPrettier.rules,
+    'vue-scoped-css/enforce-style-type': 'off',
+  },
 };
