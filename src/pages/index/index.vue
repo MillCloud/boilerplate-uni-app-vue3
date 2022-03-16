bb5de6f5b049cff3b5771835378684f04f3e05e5
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-03-16 20:43:25 +0800

    feat: update example

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,13 +1,26 @@
 <template>
-  <view class="center flex-col">
+  <view class="container items-center justify-center">
     <image
-      class="my-20 mx-auto block h-20 w-20"
+      class="m-4 mx-auto block h-20 w-20"
       src="/static/logo.png"
     />
     <view class="flex justify-center">
       <text class="text-xl">{{ title }}</text>
     </view>
-    <button>test</button>
+    <view class="flex justify-between">
+      <button
+        class="m-4 rounded border border-solid border-gray-300 bg-gray-100 px-4 transition"
+        hover-class="bg-primary border-primary text-white"
+      >
+        Default Button
+      </button>
+      <button
+        class="m-4 rounded border border-solid border-primary bg-primary px-4 text-white transition"
+        hover-class="bg-primary-darken-1 border-primary-darken-1"
+      >
+        Primary Button
+      </button>
+    </view>
   </view>
 </template>
 
@@ -17,5 +30,5 @@
 >
 import { ref } from 'vue';
 
-const title = ref('Hello');
+const title = ref('Hello UniApp');
 </script>
