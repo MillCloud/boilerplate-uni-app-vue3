263502d00c5f91bf11a5a9bd0eeb893f7136e753
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-03-17 09:47:06 +0800

    feat: tui example

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,24 +4,27 @@
       class="m-4 mx-auto block h-20 w-20"
       src="/static/logo.png"
     />
-    <view class="flex justify-center">
+    <view class="flex w-full justify-center">
       <text class="text-xl">{{ title }}</text>
     </view>
-    <view class="flex justify-between">
+    <view class="flex w-full justify-between">
       <button
-        class="m-4 rounded border border-solid border-gray-300 bg-gray-100 px-4 transition"
+        class="m-4 flex h-8 flex-auto items-center justify-center rounded border border-solid border-gray-300 bg-gray-100 px-4 transition"
         hover-class="bg-primary border-primary text-white"
       >
-        Default Button
+        default
       </button>
       <button
-        class="m-4 rounded border border-solid border-primary bg-primary px-4 text-white transition"
+        class="m-4 flex h-8 flex-auto items-center justify-center rounded border border-solid border-primary bg-primary px-4 text-white transition"
         hover-class="bg-primary-darken-1 border-primary-darken-1"
       >
-        Primary Button
+        primary
       </button>
     </view>
-    <view class="flex justify-center p-4">
+    <view class="flex w-full justify-center">
+      <tui-button class="m-4 w-full">thor-ui</tui-button>
+    </view>
+    <view class="flex w-full justify-center p-4">
       <template v-if="isLoading">Loading...</template>
       <template v-else>{{ data }}</template>
     </view>
