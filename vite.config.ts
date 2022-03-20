2110baa6238ea5e6fe0af34543fe44fb65ad4764
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-03-20 13:15:33 +0800

    chore: update postcss config

--- vite.config.ts
+++ vite.config.ts
@@ -21,8 +21,6 @@ export default defineConfig({
       plugins: [
         tailwindcss(),
         postcssPresetEnv({
-          browsers:
-            'defaults and supports es6-module and supports es6-module-dynamic-import, not opera > 0, not samsung > 0, not and_qq > 0',
           stage: 3,
         }),
       ],
