ce2033be66025afcdc2ab0cfd9e26a7744a83ae1
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-03-20 11:44:33 +0800

    fix: update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,13 @@ import path from 'path';
 // import { fileURLToPath } from 'url';
 import uni from '@dcloudio/vite-plugin-uni';
 import eslint from '@modyqyw/vite-plugin-eslint';
-// @ts-ignore
-import postcssFlexBugsFixes from 'postcss-flexbugs-fixes';
+import tailwindcss from 'tailwindcss';
 // @ts-ignore
 import postcssPresetEnv from 'postcss-preset-env';
-// @ts-ignore
-import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 import env from 'vite-plugin-env-compatible';
 import stylelint from 'vite-plugin-stylelint';
+import inspect from 'vite-plugin-inspect';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,14 +16,13 @@ export default defineConfig({
     target: 'es2015',
   },
   css: {
+    // FIX: not support postcss config file yet
     postcss: {
       plugins: [
         tailwindcss(),
-        postcssFlexBugsFixes(),
         postcssPresetEnv({
-          autoprefixer: {
-            flexbox: 'no-2009',
-          },
+          browsers:
+            'defaults and supports es6-module and supports es6-module-dynamic-import, not opera > 0, not samsung > 0, not and_qq > 0',
           stage: 3,
         }),
       ],
@@ -60,6 +57,7 @@ export default defineConfig({
     stylelint({
       fix: true,
     }),
+    inspect(),
   ],
   resolve: {
     alias: {
